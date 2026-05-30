11252dbe8b586ea30b26351e80038ec4dd0bfb06
Smu1zel <lyndenl25@yahoo.com>
2026-05-30 02:14:09 -0400

    Get vndservicemanager starting.

--- device.mk
+++ device.mk
@@ -18,4 +18,5 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.recovery.mt6761.rc:recovery/root/init.recovery.mt6761.rc \
     $(LOCAL_PATH)/manifest.xml:recovery/root/system/etc/vintf/manifest.xml \
-    $(LOCAL_PATH)/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml
+    $(LOCAL_PATH)/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml \
+    $(LOCAL_PATH)/vndservice_contexts:recovery/root/vendor/etc/selinux/vndservice_contexts
