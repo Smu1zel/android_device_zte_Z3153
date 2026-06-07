8287a94e35f901fb4590c802deab760747ab759b
Smu1zel <lyndenl25@yahoo.com>
2026-06-07 14:42:09 -0400

    Not ideal, but we'll try it anyway.

--- device.mk
+++ device.mk
@@ -23,9 +23,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-impl.recovery \
+    android.hardware.health@2.0-service
 
 # Audio
 PRODUCT_PACKAGES += \
