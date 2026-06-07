020a186acf48613841bc3bae9ce2aba65ed7560a
Smu1zel <lyndenl25@yahoo.com>
2026-06-07 07:38:07 -0400

    Should've known we'd need this.

--- device.mk
+++ device.mk
@@ -40,6 +40,8 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge.vendor \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.1-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service
 
