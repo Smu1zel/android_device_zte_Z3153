4aa369a0a2d0326fa9bf4c35ee7d3e9161b2cc09
Smu1zel <lyndenl25@yahoo.com>
2026-09-08 00:53:52 -0400

    Z3153: Modernize lunch choices and drop obsolete binder/vendorsetup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,11 @@
 #
-# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-FileCopyrightText: 2026 Lynden Lewis <lyndenl25@yahoo.com>
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_Z3153.mk \
     $(LOCAL_DIR)/lineage_Z3153.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Z3153-user \
-    lineage_Z3153-userdebug \
-    lineage_Z3153-eng
+    lineage_Z3153-trunk_staging-userdebug \
+    lineage_Z3153-trunk_staging-eng
